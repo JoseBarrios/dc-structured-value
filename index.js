70cd2cbea50f40b1c9dd13e8e95436c63a6c1724
Jose Barrios <jose@cognilab.com>
2017-06-20 17:30:44 -0700

    Supports multiple inheretance

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
+const Thing = require('@josebarrios/thing')
 const Intangible = require('@josebarrios/intangible');
+const Multiple = require('aggregation/es6');
 
-class StructuredValue extends Intangible{
-
-  constructor(model){
-    model = model || {};
-    super(model)
-  }
+class StructuredValue extends Multiple(Intangible, Thing){
+    constructor(model){
+      model = model || {};
+      super(model)
+    }
 }
 
 module.exports = StructuredValue;
--- tests/test.js
+++ tests/test.js
@@ -2,3 +2,20 @@
 
 var assert = require('assert');
 var StructuredValue = require('../index.js');
+
+
+var model = {};
+model.image = 'url';
+model.givenName = 'jose';
+model.familyName = 'barrios';
+let structuredValue = new StructuredValue(model);
+
+describe('StructuredValue\n', function() {
+  describe('Properties:', function() {
+    describe('structuredValue.image', function() {
+      it('should ...', function() {
+        assert.equal(structuredValue.image, 'url');
+      });
+    });
+  });
+});
